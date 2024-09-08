d835449833417c73469f6002767b9641c698a665
nikitt <nikittaosipov@yandex.ru>
2024-09-08 16:05:40 +0400

    маршрутизация

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,2 +1,9 @@
 from django.urls import path
 from . import views
+
+app_name = 'catalog'
+
+urlpatterns = [
+    path('', views.home, name='home'),  # Главная
+    path('contacts/', views.contacts, name='contacts')  # Контакты
+]
