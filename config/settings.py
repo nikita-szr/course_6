7bb6cf3ca1927bdf339fac01a22fd0332a0241b8
nikitt <nikittaosipov@yandex.ru>
2024-09-08 16:36:31 +0400

    settings update

--- config/settings.py
+++ config/settings.py
@@ -109,6 +109,7 @@ LANGUAGE_CODE = "en-us"
 TIME_ZONE = "Europe/Samara"
 
 USE_I18N = True
+USE_L18N = True
 
 USE_TZ = True
 
@@ -117,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
