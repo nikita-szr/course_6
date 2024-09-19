e7d3a15712924c800dcf4deb16ccecb74d65f406
nikitt <nikittaosipov@yandex.ru>
2024-09-19 22:05:28 +0400

    шаблон base шаблон menu

--- catalog/urls.py
+++ catalog/urls.py
@@ -8,7 +8,7 @@ app_name = 'catalog'
 urlpatterns = [
     path('home/', views.home, name='home'),  # Главная
     path('contacts/', views.contacts, name='contacts'),  # Контакты
-    path('product/<int:product_id>/', views.product_info, name='product_info')  # Информация о товаре
+    path('product/<int:product_id>/', views.product_info, name='product_info'),  # Информация о товаре
 ]
 
 if settings.DEBUG:
