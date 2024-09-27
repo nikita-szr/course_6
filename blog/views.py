32d71a02aca809e82cc34a9cc9fb23b07340222a
nikitt <nikittaosipov@yandex.ru>
2024-09-27 23:48:55 +0400

    Модифицирован вывод и обработка запросов

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import BlogPost
 from .forms import BlogPostForm
@@ -9,10 +9,19 @@ class BlogPostListView(ListView):
     template_name = 'blog/blogpost_list.html'
     context_object_name = 'posts'
 
+    def get_queryset(self):
+        return BlogPost.objects.filter(is_published=True)
+
 
 class BlogPostDetailView(DetailView):
     model = BlogPost
     template_name = 'blog/blogpost_detail.html'
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        obj.view_count += 1
+        obj.save()
+        return obj
 
 
 class BlogPostCreateView(CreateView):
@@ -27,6 +36,9 @@ class BlogPostUpdateView(UpdateView):
     form_class = BlogPostForm
     template_name = 'blog/blogpost_form.html'
     success_url = reverse_lazy('blog:blogpost_list')
+
+    def get_success_url(self):
+        return reverse('blogpost_detail', kwargs={'pk': self.object.pk})
 
 
 class BlogPostDeleteView(DeleteView):
