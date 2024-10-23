80f5c6fef553b9e796298f42347746028993960b
nikitt <nikittaosipov@yandex.ru>
2024-10-23 22:05:52 +0400

    кэширование списка продуктов в категории

--- catalog/services.py
+++ catalog/services.py
@@ -1,12 +1,21 @@
 from .models import Product, Category
+from django.core.cache import cache
 
 
 class ProductService:
 
     @staticmethod
     def get_products_by_category(category_id):
+        cache_key = f'products_category_{category_id}'
 
-        return Product.objects.filter(category_id=category_id)
+        products = cache.get(cache_key)
+        if not products:
+
+            products = Product.objects.filter(category_id=category_id)
+
+            cache.set(cache_key, products, timeout=60 * 15)
+
+        return products
 
     @staticmethod
     def get_category_name(category_id):
